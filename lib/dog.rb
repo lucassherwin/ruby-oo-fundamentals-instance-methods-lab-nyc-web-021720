1283c4412c70eee51f900c470cbf0bb087dcaf91
lucassherwin <lksherwin@gmail.com>
2020-02-19 15:09:13 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+#class dog
+class Dog
+    #instance method 'bark' -> puts "Woof!"
+    def bark
+        puts "Woof!"
+    end
+    #instance method 'sit' -> puts "The Dog is sitting"
+    def sit
+        puts "The Dog is sitting"
+    end
+end
